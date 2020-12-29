20e1c807b1d8d183287871ba445c57479c7f03d7
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-30 02:39:56 +0800

    tiny example for puppet

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,60 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-puppet'
+import { useState, State } from 'react-puppet'
 import 'react-puppet/dist/index.css'
+const todos = new State([]);
+const done = new State([]);
+const editingTodo = new State("");
+
+const NewTodo = () => {
+  const text = useState(editingTodo)
+  const todoList = useState(todos)
+  return (<div>
+    <input type="text" value={text} onChange={
+      e => editingTodo.set(e.target.value)
+    }/>
+    <button onClick={e => {
+      todos.set([text, ...todoList])
+      editingTodo.set('')
+    }}>add</button>
+  </div>)
+}
+
+const Item = ({tid, text}) => {
+  const todoList = useState(todos)
+  const doneList = useState(done)
+  return <div>
+    <button onClick={e => {
+      const target = todoList[tid];
+      let arr = [...todoList]
+      arr.splice(tid, 1)
+      todos.set(arr).then(()=>{
+        return done.set([target, ...doneList])
+      })
+    }}>done</button>
+    <button onClick={e => {
+      let arr = [...todoList]
+      arr.splice(tid, 1)
+      todos.set(arr)
+    }}>delete</button>
+    <div style={{display: 'inline-block'}}>{text}</div>
+  </div>
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const todoList = useState(todos)
+  const doneList = useState(done)
+  return <div>
+    <NewTodo />
+    <div> TODO: </div>
+    {
+      todoList.map((text, i) => <Item key={i} tid={i} text={text}/>)
+    }
+    <div> DONE: </div>
+    {
+      doneList.map((text, i) => <div key={i}>{text}</div>)
+    }
+  </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class State {
+  constructor(initial, after, debug_id) {
+    this.val = initial
+    this.setters = new Set()
+    this.afters = []
+    this.debug_id = debug_id
+    if(after) {
+      this.afters.push(after)
+    }
+  }
+
+  link = (setter) => {
+    this.setters.add(setter)
+  }
+  unlink = (setter) => {
+    this.setters.delete(setter)
+  }
+  set = async (val) => {
+    for(let setter of this.setters) {
+      await setter(val)
+    }
+    this.val = val
+    for(let after of this.afters) {
+      await after(val)
+    }
+  }
 }
+const useState = (mcbinder, debug_context) => {
+  const [val, setter] = React.useState(mcbinder.val);
+  const cbRef = React.useRef(null)
+  const setThen = (val) => {
+    return new Promise((resolve, reject) =>{
+      cbRef.current = resolve;
+      setter(val)
+    })
+  }
+  React.useEffect(()=> {
+    mcbinder.link(setThen)
+    return () => {
+      mcbinder.unlink(setThen)
+    }
+  }, [])
+  React.useEffect(()=> {
+    if(cbRef.current) {
+      cbRef.current(val);
+      cbRef.current = null;
+    }
+  }, [val])
+  return val;
+}
+
+export { useState, State }
